4aeb92c368392cb15119efa0672a0c1c12578df6
cherylcherry <cherylcherry95@gmail.com>
2015-06-22 23:00:19 +0530

    changed the color of pie charts

--- skill.js
+++ skill.js
@@ -4,7 +4,7 @@ $(function () {
                 onStep: function (from, to, percent) {
                     $(this.el).find('.percent').text(Math.round(percent));
                 },
-                barColor: '#05D6AC', //FORCOLOR OF CHART
+                barColor: 'rgb(217, 156, 91)', //FORCOLOR OF CHART
                 lineWidth: 10, //WIDTH OF STRIP OF SKILL CHART
                 size: 200, //WIDTH - HEIGHT OF SKILL CHART(SHOULD BE IN SQUARE) -  SHOUL BE EQUAL - Like 200px 
             });
